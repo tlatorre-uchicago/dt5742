10161fbaad5d4bd9e405317b14a885a8f96534ff
tlatorre <tlatorre@uchicago.edu>
2021-12-14 14:16:50 -0800

    update

--- wavedump-3.10.3/src/btl_qa.sql
+++ wavedump-3.10.3/src/btl_qa.sql
@@ -13,3 +13,43 @@ CREATE TABLE btl_qa (
     ch_511              real[],
     institution         inst DEFAULT 'Caltech'::inst NOT NULL
 );
+
+-- create btl admin user
+CREATE ROLE btl_admin WITH LOGIN;
+
+-- btl admin user has all rights to everything in the database
+GRANT ALL ON btl_qa TO btl_admin;
+GRANT ALL ON SEQUENCE btl_qa_key_seq TO btl_admin;
+
+-- create btl read user
+CREATE ROLE btl_read;
+
+-- btl read user has select rights on everything in the btl schema
+GRANT SELECT ON btl_qa TO detector_read;
+GRANT SELECT ON SEQUENCE btl_qa_key_seq TO detector_read;
+
+-- create btl write user
+CREATE ROLE btl_write;
+
+-- btl write user has select rights on everything in the btl schema
+GRANT SELECT, INSERT ON btl_qa TO detector_read;
+GRANT SELECT, USAGE ON SEQUENCE btl_qa_key_seq TO detector_read;
+
+-- create btl user
+CREATE ROLE btl WITH LOGIN;
+
+-- this is the role used by the assembly centers to upload data
+GRANT btl_read, btl_write TO btl;
+
+-- create cms user
+CREATE ROLE cms WITH LOGIN;
+
+-- this is the role used by the website, should only have read access
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO snoplus;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO snoplus;
+
+-----------------------------
+-- Set up ownership of tables
+-----------------------------
+
+ALTER TABLE btl_qa OWNER TO btl_admin;
